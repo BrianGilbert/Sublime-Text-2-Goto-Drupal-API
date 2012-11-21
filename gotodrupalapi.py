e84925ff45f6b09588ef2194ebe54035c02a2f80
Donovan Tengblad <purplefish32@gmail.com>
2012-11-21 01:17:34 +0100

    Remove unused import Add required spacing

--- gotodrupalapi.py
+++ gotodrupalapi.py
@@ -1,10 +1,10 @@
-import sublime
 import sublime_plugin
 import webbrowser
 
 # -------------------------------------------
 # { "keys": ["super+shift+a"], "command": "open_drupalapi" }
 # -------------------------------------------
+
 
 # Open the URL in the current selection
 class GotoDrupalapiCommand(sublime_plugin.TextCommand):
